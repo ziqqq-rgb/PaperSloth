6f0fd21fa10a712daf0da0f0514d03d545dbe667
raziq <raziqsufian2002@gmail.com>
2026-06-03 10:54:27 +0800

    frontend

---
+++ frontend/src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// You'll need to create an App.tsx file next if you don't have one!
+// import App from './App.tsx' 
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <div>Hello PaperSloth!</div>
+  </React.StrictMode>,
+)
